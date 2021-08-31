74c926780a87969605fe7526926d7093253f9a49
Patrick Gillan <pgillan@minorimpact.com>
2021-08-31 02:17:38 -0400

    0.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='minorimpact',
     packages=find_packages(include=['minorimpact']),
-    version='0.0.4',
+    version='0.0.5',
     description='Personal utility library',
     author='Patrick Gillan',
     author_email = 'pgillan@minorimpact.com',
