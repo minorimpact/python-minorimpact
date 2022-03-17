2f33f8c92e78e95c988735ec81a70e30fd5652e4
Patrick Gillan <pgillan@gmail.com>
2022-03-17 07:23:59 +0200

    Updated setup file with classifiers.

--- setup.py
+++ setup.py
@@ -7,17 +7,26 @@ with open('./README.md', encoding='utf-8') as f:
     readme = f.read()
 
 setup(
-    name='minorimpact',
-    packages=find_packages(include=['minorimpact']),
-    version=minorimpact.__version__,
-    description='Personal utility library',
-    long_description = readme,
-    long_description_content_type = 'text/markdown',
     author='Patrick Gillan',
     author_email = 'pgillan@minorimpact.com',
-    license='GPLv3',
+    classifiers = [
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
+        ],
+    description='Personal utility library',
     install_requires=['psutil'],
+    license='GPLv3',
+    long_description = readme,
+    long_description_content_type = 'text/markdown',
+    name='minorimpact',
+    packages=find_packages(include=['minorimpact']),
     setup_requires=[],
     tests_require=[],
     url = "https://github.com/minorimpact/python-minorimpact",
+    version=minorimpact.__version__,
 )
